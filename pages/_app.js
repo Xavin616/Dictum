5c0e53cf84151223e7b5730e0c0cd09feb7a4764
jacexavinbello@gmail.com <Xavin616>
2023-01-07 10:30:37 +0100

    Dictum Commit #7: Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,29 @@ import '../styles/globals.css';
 import { MantineProvider } from '@mantine/core';
 import Layout from '../components/Layout';
 import { NotificationsProvider } from '@mantine/notifications';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export default function App({ Component, pageProps }) {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colors: {
-          cyan: ["#652B19", "#7B341E", "#9C4221", "#C05621", "#DD6B20", "#ED8936", "#F6AD55", "#FBD38D", "#FEEBC8", "#FFFAF0"]
-        },
-        primaryColor: 'orange',
-        colorScheme: 'light'
-      }}
-      > 
-      <NotificationsProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </NotificationsProvider>
-    </MantineProvider>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colors: {
+            cyan: ["#652B19", "#7B341E", "#9C4221", "#C05621", "#DD6B20", "#ED8936", "#F6AD55", "#FBD38D", "#FEEBC8", "#FFFAF0"]
+          },
+          primaryColor: 'orange',
+          colorScheme: 'light'
+        }}
+        > 
+        <NotificationsProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </NotificationsProvider>
+      </MantineProvider>
+    </>
   )
 }
